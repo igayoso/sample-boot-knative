b02844801c7ad07b8c3f4d2315e161af8600d5f4
Israel Gayoso Perez <igayoso@gmail.com>
2019-06-13 12:59:06 -0400

    Demo

--- applications/sample-boot-knative-app/src/main/kotlin/sample/meter/MessageHandler.kt
+++ applications/sample-boot-knative-app/src/main/kotlin/sample/meter/MessageHandler.kt
@@ -24,7 +24,7 @@ class MessageHandler {
             logger.info("Handling message: {}", m)
             counter.increment()
             Mono
-                    .fromCallable { MessageAck(id = m.id, received = m.payload, ack = "ack", hello = "World!") }
+                    .fromCallable { MessageAck(id = m.id, received = m.payload, ack = "ack", hello = "DevOps!") }
                     .delayElement(Duration.ofMillis(m.delay))
                     .flatMap { messageAck ->
                         if (m.throwException) {
