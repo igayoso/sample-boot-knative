8df64501521216868f6edfc112ada385ab37f15b
Miguel Sauza <miguel.sauza@gmail.com>
2019-06-19 11:47:03 -0400

    Add simple-reactive-app

---
+++ simple-reactive-app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.jetbrains.kotlin.jvm") version "1.3.31"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	id("org.springframework.boot") version "2.2.0.M3"
+}
+
+group = "com.sample"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+	implementation("org.springframework.fu:spring-fu-kofu:0.1")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	maven("https://repo.spring.io/milestone")
+	maven("https://repo.spring.io/snapshot")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		jvmTarget = "1.8"
+		freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=enable")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+configurations.all {
+	exclude(module = "jakarta.validation-api")
+	exclude(module = "hibernate-validator")
+	if (project.hasProperty("graal")) {
+		exclude(module = "netty-transport-native-epoll")
+		exclude(module = "netty-transport-native-unix-common")
+		exclude(module = "netty-codec-http2")
+	}
+}
